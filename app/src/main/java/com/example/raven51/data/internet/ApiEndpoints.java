c7ff682979904e1bcd180f96e49affe25d65a8e0
RavenRav <akilaia@inbox.ru>
2020-01-19 21:35:16 +0300

    3

--- app/src/main/java/com/example/raven51/data/internet/ApiEndpoints.java
+++ app/src/main/java/com/example/raven51/data/internet/ApiEndpoints.java
@@ -2,4 +2,5 @@ package com.example.raven51.data.internet;
 
 public class ApiEndpoints {
     public static final String FORECAST = "data/2.5/forecast";
+    public static final String CURRENT = "data/2.5/weather";
 }
--- app/src/main/java/com/example/raven51/data/internet/RetrofitService.java
+++ app/src/main/java/com/example/raven51/data/internet/RetrofitService.java
@@ -2,21 +2,26 @@ package com.example.raven51.data.internet;
 
 
 import com.example.raven51.data.entity.current.CurrentWeather;
+import com.example.raven51.data.entity.forecast.ForecastEntity;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+import static com.example.raven51.data.internet.ApiEndpoints.CURRENT;
 import static com.example.raven51.data.internet.ApiEndpoints.FORECAST;
 
 public interface RetrofitService {
 
 
-    @GET("data/2.5/weather")
+    @GET(CURRENT)
     Call<CurrentWeather> fetchCurrentWeather(@Query("q") String city,
                                              @Query("appid") String appId,
                                              @Query("units") String metric,
                                              @Query("dt") String UTC
                                              );
 }
+//    Call<ForecastEntity>getForecast(@Query("q") String city,
+//                                    @Query("appid") String appId,
+//                                    @Query("units") String metric);
 
